a1364e50677c8020d4f035eac032b827b977c50a
Craig Hockenberry <craig.hockenberry@gmail.com>
2024-01-29 17:57:11 -0800

    Facet byte positions are now handled correctly.

--- Plugins/social.bsky/plugin.js
+++ Plugins/social.bsky/plugin.js
@@ -19,7 +19,7 @@ function identify() {
 const uriPrefix = "https://bsky.app";
 
 function load() {
-	sendRequest(site + "/xrpc/app.bsky.feed.getTimeline?algorithm=reverse-chronological&limit=22")
+	sendRequest(site + "/xrpc/app.bsky.feed.getTimeline?algorithm=reverse-chronological&limit=100")
 	.then((text) => {
 		const jsonObject = JSON.parse(text);
 
@@ -36,22 +36,40 @@ function load() {
 			
 			const date = new Date(record.createdAt);
 			
-			var content = record.text.replaceAll("\n", "\n\r");
+			var content = record.text;
+			
+			// NOTE: Facets are a pain in the butt since they use byte positions in UTF-8. The JSON parser generates UTF-16
+			// so we have to convert it back to bytes, find what we need, and then make a new UTF-16 string.
 			
 			if (record.facets != null) {
 				// NOTE: Done in reverse order because we're modifying string in place.
 				for (const facet of record.facets.reverse()) {
-					if (facet.features.length > 0 && facet.features[0].$type == "app.bsky.richtext.facet#link") {
-						const prefix = content.substring(0, facet.index.byteStart);
-						const suffix = content.substring(facet.index.byteEnd);
-						const text = content.substring(facet.index.byteStart, facet.index.byteEnd);
-						const link = "<a href=\"" + facet.features[0].uri + "\">" + text + "</a>";
-						content = prefix + link + suffix;
+					if (facet.features.length > 0) {
+						const bytes = stringToBytes(content);
+						
+						const prefixBytes = bytes.slice(0, facet.index.byteStart);
+						const suffixBytes = bytes.slice(facet.index.byteEnd);
+						const textBytes = bytes.slice(facet.index.byteStart, facet.index.byteEnd);
+
+						const prefix = bytesToString(prefixBytes);
+						const suffix = bytesToString(suffixBytes);
+						const text = bytesToString(textBytes);
+
+						const feature = facet.features[0];
+
+						if (feature.$type == "app.bsky.richtext.facet#link") {
+							const link = "<a href=\"" + feature.uri + "\">" + text + "</a>";
+							content = prefix + link + suffix;
+						}
+						else if (feature.$type == "app.bsky.richtext.facet#mention") {
+							const link = "<a href=\"" + uriPrefix + "/profile/" + feature.did + "\">" + text + "</a>";
+							content = prefix + link + suffix;
+						}
 					}
 				}
 			}
 
-			content = "<p>" + content.replaceAll("\n\r\n\r", "<br/>") + "</p>";
+			content = "<p>" + content.replaceAll("\n\n", "<br/>") + "</p>";
 
 			var attachments = null;
 			if (item.post.embed != null) {
@@ -90,3 +108,44 @@ function load() {
 		processError(requestError);
 	});	
 }
+
+function stringToBytes(text) {
+	// the encoded text is in UTF-8 with percent escapes for characters other than: A–Z a–z 0–9 - _ . ! ~ * ' ( )
+	const encodedText = encodeURIComponent(text);
+
+	var resultArray = [];
+
+	for (var i = 0; i < encodedText.length; i++) {
+		const character = encodedText[i];
+		if (character == "%") {
+			// convert the hex encoding to an integer value
+			const hex = encodedText.substring(i+1, i+3);
+			const byte = parseInt(hex, 16);
+			resultArray.push(byte);
+			
+			// skip over the characters we just consumed
+			i += 2;
+	  	}
+	  	else {
+	  		// convert the unencoded character to an integer value
+			const byte = character.charCodeAt(0);
+			resultArray.push(byte);
+	  	}
+	}
+
+	return resultArray;
+}
+
+function bytesToString(bytes) {
+	// map all integer bytes to their percent escape equivalents
+	const hexes = bytes.map((element) => {
+		return "%" + element.toString(16).padStart(2, "0").toUpperCase();
+	})
+	const text = hexes.join("");
+
+	// convert the percent escaped UTF-8 to UTF-16
+	const resultString = decodeURIComponent(text);
+	
+	return resultString;
+}
+
