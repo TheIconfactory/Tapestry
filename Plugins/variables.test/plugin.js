c77b5468204819522a25ba31c47bbd88a5d05fe9
Craig Hockenberry <craig.hockenberry@gmail.com>
2024-04-26 16:58:59 -0700

    Added a blockquote test.

--- Plugins/variables.test/plugin.js
+++ Plugins/variables.test/plugin.js
@@ -20,7 +20,7 @@ function load() {
 	}
 	status += ` at level ${value}`;
 	
-	let content = `<p>Test content for ${site} & ${title}</p><p>${status}</p><p>For dessert, you'll be having ${dessert_choice}. Enjoy!</p><p><a href="https://streamer.iconfactory.net">Check user agent in logs</a></p>`
+	let content = `<p>Test content for ${site} & ${title}</p><p>${status}</p><blockquote><p>For dessert, you'll be having ${dessert_choice}. Enjoy!</p></blockquote><p><a href="https://streamer.iconfactory.net">Check user agent in logs</a></p>`
 	
 	const post = Post.createWithUriDateContent(uri, date, content);
 
