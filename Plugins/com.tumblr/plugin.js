8ce294faa319d5c6e3bba20284c0686fd653c8ff
Craig Hockenberry <craig.hockenberry@gmail.com>
2024-06-07 17:20:12 -0700

    Added annotations to Tumblr posts.

--- Plugins/com.tumblr/plugin.js
+++ Plugins/com.tumblr/plugin.js
@@ -31,20 +31,47 @@ function load() {
 		var results = [];
 		for (const item of items) {
 			const blog = item.blog;
-			const identity = Identity.createWithName(blog["name"]);
-			identity.uri = blog["url"];
-			identity.avatar = "https://api.tumblr.com/v2/blog/" + blog["name"] + "/avatar/96";
+			const identity = Identity.createWithName(blog.name);
+			identity.uri = blog.url;
+			identity.username = blog.title;
+			identity.avatar = "https://api.tumblr.com/v2/blog/" + blog.name + "/avatar/96";
 
-			const uri = item["post_url"];
-			const date = new Date(item["timestamp"] * 1000); // timestamp is seconds since the epoch, convert to milliseconds
+			const uri = item.post_url;
+			const date = new Date(item.timestamp * 1000); // timestamp is seconds since the epoch, convert to milliseconds
 			
 			// TODO: Add an annotation for reblogging.
+
+			// item.tags = ["tweets", "white people twitter", "funny tweets"]
+			// linked as: https://www.tumblr.com/tagged/white%20people%20twitter
+			
+			// parent_post_url != null if a reblog
+
+			let lastTrail = null;
+			if (item.parent_post_url != null) {
+				// this is a reblog
+				if (item.trail != null && item.trail.length > 0) {
+					lastTrail = item.trail[0];
+				}
+			}
+			
+			let annotation = null;
+			if (lastTrail != null) {
+				const userName = lastTrail.blog.name;
+				const text = "Reblogged " + userName;
+				annotation = Annotation.createWithText(text);
+				annotation.uri = item.parent_post_url;
+			}
 			
 			let content = null;
 			let attachments = null;
-			if (item["type"] == "photo") {
-				content = item["caption"];
-				
+			if (item.type == "photo") {
+				if (lastTrail != null) {
+					content = lastTrail.content;
+				}
+				else {
+					content = item.caption;
+				}
+							
 				attachments = [];
 				let photos = item.photos;
 				let count = Math.min(4, photos.length);
@@ -52,12 +79,28 @@ function load() {
 					let photo = photos[index];
 					const media = photo.original_size.url;
 					const attachment = Attachment.createWithMedia(media);
-					attachment.text = item["summary"];
+					attachment.text = item.summary;
 					attachments.push(attachment);
 				}
 			}
-			else if (item["type"] == "text") {
-				content = item["body"];
+			else if (item.type == "video") {
+				if (lastTrail != null) {
+					content = lastTrail.content;
+				}
+				else {
+					content = item.body;
+				}
+			}
+			else if (item.type == "text") {
+				if (lastTrail != null) {
+					content = lastTrail.content;
+				}
+				else {
+					content = item.body;
+				}
+			}
+			else {
+				console.log(`ignored ${item.type}`);
 			}
 			
 			if (content != null) {
@@ -66,6 +109,9 @@ function load() {
 				post.author = identity;
 				if (attachments != null) {
 					post.attachments = attachments
+				}
+				if (annotation != null) {
+					post.annotations = [annotation];
 				}
 				results.push(post);
 			}
