9e893e36bab8edd4c2950858c70bdf1e3b74a752
Craig Hockenberry <craig.hockenberry@gmail.com>
2024-08-01 17:14:41 -0700

    Directory of satellite images is now used for url and date.

--- Plugins/gov.noaa.nesdis.star/plugin.js
+++ Plugins/gov.noaa.nesdis.star/plugin.js
@@ -39,15 +39,46 @@ function load() {
 			imageWidth = "1808";
 			imageHeight = "1808";
 		}
-		let url = `https://cdn.star.nesdis.noaa.gov/${directory}/ABI/${subdirectory}/${image}/${imageWidth}x${imageHeight}.jpg`;
-		
-		const date = new Date();
-		const content = `<p><img src="${url}" width="${imageWidth}" height="${imageHeight}"/></p><p>${image} image of Continental US (${satellite})</p>`;
-		var resultItem = Item.createWithUriDate(url, date);
-		resultItem.body = content;
-		
-		processResults([resultItem]);
-		
+		let directoryUrl = `https://cdn.star.nesdis.noaa.gov/${directory}/ABI/${subdirectory}/${image}/`;
+		sendRequest(directoryUrl)
+		.then((html) => {
+			
+			let imageRegex = null;
+			if (view == "Full Disk") {
+				imageRegex = /<a href="(.*-1808x1808\.jpg)">/gm
+			}
+			else {
+				imageRegex = /<a href="(.*-2500x1500\.jpg)">/gm
+			}
+			const matches = [...html.matchAll(imageRegex)];
+			const lastMatch = matches[matches.length - 1];
+			const imageUrl = lastMatch[1];
+
+			let year = parseInt(imageUrl.substring(0, 4));
+			let dayOfYear = parseInt(imageUrl.substring(4, 7));
+			let hour = parseInt(imageUrl.substring(7, 9));
+			let minute = parseInt(imageUrl.substring(9, 11));
+			
+			const url = `https://cdn.star.nesdis.noaa.gov/${directory}/ABI/${subdirectory}/${image}/${imageUrl}`;
+			
+			const initialDate = new Date(Date.UTC(year, 0, 1, hour, minute));
+			const date = new Date(initialDate.setUTCDate(dayOfYear));
+			
+			let attachment = MediaAttachment.createWithUrl(url);
+			attachment.aspectSize = {width: imageWidth, height: imageHeight};
+			
+			const content = `<p>${image} image of Continental US (${satellite})</p>`;
+			var resultItem = Item.createWithUriDate(url, date);
+			resultItem.body = content;
+			resultItem.attachments = [attachment];
+			
+			processResults([resultItem]);
+			
+		})
+		.catch((requestError) => {
+			processError(requestError);
+		});	
+
 		lastDate = date;
 	}
 }
