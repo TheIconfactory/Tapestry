60ac66850b101ca926c361220558cbd68f66862b
Craig Hockenberry <craig.hockenberry@gmail.com>
2024-08-05 18:03:28 -0700

    Started working on retrieving batches of posts from Tumblr.

--- Plugins/com.tumblr/plugin.js
+++ Plugins/com.tumblr/plugin.js
@@ -23,9 +23,164 @@ function verify() {
 	});
 }
 
-const imageRegex = /<img src="([^"]*)/;
+function postForItem(item) {
+	const blog = item.blog;
+	const identity = Identity.createWithName(blog.name);
+	identity.uri = blog.url;
+	identity.username = blog.title;
+	identity.avatar = "https://api.tumblr.com/v2/blog/" + blog.name + "/avatar/96";
+
+	const uri = item.post_url;
+	const date = new Date(item.timestamp * 1000); // timestamp is seconds since the epoch, convert to milliseconds
+	
+	// item.tags = ["tweets", "white people twitter", "funny tweets"]
+	// linked as: https://www.tumblr.com/tagged/white%20people%20twitter
+	
+	// parent_post_url != null if a reblog
+
+	let lastTrail = null;
+	if (item.parent_post_url != null) {
+		// this is a reblog
+		if (item.trail != null && item.trail.length > 0) {
+			lastTrail = item.trail[0];
+		}
+	}
+	
+	let annotation = null;
+	if (lastTrail != null) {
+		const userName = lastTrail.blog.name;
+		const text = "Reblogged " + userName;
+		annotation = Annotation.createWithText(text);
+		annotation.uri = item.parent_post_url;
+	}
+	
+	let content = null;
+	let attachments = null;
+	if (item.type == "photo") {
+		if (lastTrail != null) {
+			content = lastTrail.content;
+		}
+		else {
+			content = item.caption;
+		}
+					
+		attachments = [];
+		let photos = item.photos;
+		let count = photos.length;
+		for (let index = 0; index < count; index++) {
+			let photo = photos[index];
+			const media = photo.original_size.url;
+			const attachment = MediaAttachment.createWithUrl(media);
+			attachment.text = item.summary;
+			attachment.mimeType = "image";
+			attachments.push(attachment);
+		}
+	}
+	else if (item.type == "video") {
+		if (lastTrail != null) {
+			content = lastTrail.content;
+		}
+		else {
+			content = item.body;
+		}
+	}
+	else if (item.type == "text") {
+		if (lastTrail != null) {
+			content = lastTrail.content;
+		}
+		else {
+			content = item.body;
+		}
+	}
+	else {
+		console.log(`ignored item.type = ${item.type}`);
+	}
+	
+	if (content != null) {
+		const post = Item.createWithUriDate(uri, date);
+		post.body = content;
+		post.author = identity;
+		if (attachments != null) {
+			post.attachments = attachments
+		}
+		if (annotation != null) {
+			post.annotations = [annotation];
+		}
+		return post;
+	}
+	else {
+		return null;
+	}
+}
+
+function queryDashboard(doIncrementalLoad) {
+
+	return new Promise((resolve, reject) => {
+
+		// this function is called recursively to load & process batches of posts into a single list of results
+		function requestToId(id, doIncrementalLoad, resolve, reject, limit = 5, results = []) {
+			let url = null
+			if (id == null) {
+				url = `${site}/v2/user/dashboard?limit=20`;
+			}
+			else {
+				const offset = (5 - limit) * 20;
+				url = `${site}/v2/user/dashboard?limit=20&offset=${offset}`;
+			}
+			
+			console.log(`doIncrementalLoad = ${doIncrementalLoad}, id = ${id}`);
+			
+			sendRequest(url, "GET")
+			.then((text) => {
+				//console.log(text);
+				let lastId = null;
+				
+				const jsonObject = JSON.parse(text);
+				const items = jsonObject.response.posts;
+				for (const item of items) {
+					const post = postForItem(item);
+					if (post != null) {
+						results.push(post);
+						lastId = item["id"];
+					}
+				}
+				
+				const newLimit = limit - 1;
+				
+				if (lastId != null && newLimit > 0 && doIncrementalLoad == false) {
+					requestToId(lastId, doIncrementalLoad, resolve, reject, newLimit, results);
+				}
+				else {
+					resolve(results);
+				}
+			})
+			.catch((error) => {
+				reject(error);
+			});	
+		}
+
+		requestToId(null, doIncrementalLoad, resolve, reject);
+
+	});
+	
+}
+
+var doIncrementalLoad = false;
 
 function load() {
+	queryDashboard(doIncrementalLoad)
+	.then((results) =>  {
+		console.log(`finished dashboard`);
+		processResults(results, true);
+		doIncrementalLoad = true;
+	})
+	.catch((requestError) => {
+		console.log(`error dashboard`);
+		processError(requestError);
+		doIncrementalLoad = false;
+	});	
+
+/*
 	sendRequest(site + "/v2/user/dashboard")
 	.then((text) => {
 		const jsonObject = JSON.parse(text);
@@ -123,5 +278,6 @@ function load() {
 	})
 	.catch((requestError) => {
 		processError(requestError);
-	});	
+	});
+*/
 }
