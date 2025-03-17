f755dd14f461a9ad89f659649f4580cee46ceffe
Craig Hockenberry <craig.hockenberry@gmail.com>
2025-03-17 16:41:10 -0700

    Testing entity encoding.

--- Plugins-Debug/variables.test/plugin.js
+++ Plugins-Debug/variables.test/plugin.js
@@ -32,10 +32,10 @@ function load() {
 			if (status.length > 0) {
 				status += " and ";
 			}
-			status += "TURBO is engaged";
+			status += "<TURBO is engaged>";
 		}
 		status += ` at level ${value}`;
-		
+		status = status.replaceAll("<", "&lt;").replaceAll(">", "&gt;");
 		let content = `<p>Test content for ${site} & ${title}</p><p>${status}</p><blockquote><p>For dessert, you'll be having ${dessert_choice}. Enjoy!</p></blockquote><p><a href="https://streamer.iconfactory.net">Check user agent in logs</a></p>`
 		
 		const resultItem = Item.createWithUriDate(uri, date);
